0ef7bf60dd51acd5e83d3cea6968ac09d2ca72cc
jayminraval2908 <ravaljaymin2908@gmail.com>
2026-04-15 10:48:18 +0530

    update CORS

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const cors = require("cors")
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://vehicle-vault-frontend-lake.vercel.app/  "
+    "https://vehicle-vault-frontend-lake.vercel.app  "
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
